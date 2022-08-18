b9a119e8803fc3d4606a4fa9f851570682eec7eb
Abhi Saxena <71825717+tusharsaxena01@users.noreply.github.com>
2022-08-18 23:40:39 +0530

    Update Insta_Profile_Pic_Downloader.py

--- Insta_Profile_Pic_Downloader.py
+++ Insta_Profile_Pic_Downloader.py
@@ -1,17 +1,19 @@
 def profilePicDownloader(bot):
-  username = input("Enter Instagram Username: @")
-  bot.download_profile(username.profile, profile_pic_only=True)
+    username = input("Enter Instagram Username: @")
+    bot.download_profile(username, profile_pic_only=True)
 try:
     import instaloader as ins
     profilePicDownloader(ins.Instaloader())
 except ModuleNotFoundError:
     print("Module Not Found!")
-    if 'Yy' in input("Install Now? (Y\N):"):
-      import os
-      os.system('pip install instaloader')
-      import instaloader as ins
-      profilePicDownloader(ins.Instaloader())
+    ch = input("Install Now? (Y\\N): ")
+    if 'Yy' in ch:
+        import os
+        os.system('pip install instaloader')
+        import instaloader as ins
+        profilePicDownloader(ins.Instaloader())
     else:
-      print("Installation of module is required for the program to continue!")
-except:
-  print("Error! Try Again!")
+        print("Installation of module is required for the program to continue!")
+except Exception as e:
+    print("Error! Try Again!")
+    print(e)
